7d5e346e6f9efa6df27cdcf2a3e4ec4b7f422963
Didier Hoarau <didier.hoarau.dev@gmail.com>
2017-06-17 06:59:52 +0800

    Switch to Angular starter project

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,7 @@
 import { Component } from "@angular/core";
 
 @Component({
-  selector: "my-app",
-  template: `
-    <ActionBar title="My App"></ActionBar>
-    <!-- Your UI components go here -->
-  `
+    selector: "ns-app",
+    templateUrl: "app.component.html",
 })
-export class AppComponent {
-  // Your TypeScript logic goes here
-}
+export class AppComponent { }
